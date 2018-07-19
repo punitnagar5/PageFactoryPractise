4241c73af9e1157d30913fa0fb9174a53cf5afe0
punitnagar5 <puneetnagar@qainfotech.com>
2018-07-19 11:29:22 +0530

    add done

---
+++ src/main/java/MainClassPratise.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+
+public class MainClassPratise {
+	WebDriver driver;
+	
+	
+public MainClassPratise()
+{
+	this.driver = driver;
+}
+
+public void openHris(String Url)
+{
+	driver.get(Url);
+	
+}
+	
+}
